0e82f87ff6143be58ffa2658ebcdb03b93954ee1
angel sabu <angelsabu314@gmail.com>
2024-07-25 09:27:08 +0530

    First Day

---
+++ script.js
@@ -0,0 +1,32 @@
+// const button = document.getElementById('eventbtn');
+// const text = document.getElementById('text');
+// button.addEventListener('click',function(){
+//     text.innerHTML ='this is a changed text'
+// });
+// const persons ={
+//     firstName: "Kevin",
+//     lastName:"Doe",
+//     age:22,
+//     eyecolor:'blue'
+// }
+// console.log(persons.firstName +" " + "is" + " "+ persons.age + " years old")
+
+// for (let i=0; i<=10; i++) {
+//     console.log("looping through "+i)
+// }
+
+// function square(num)
+// {
+//     return num*num;
+// }
+// console.log(square(2));
+// let square=(num)=>{
+//     return num*num;
+// }
+// console.log(square(2));
+
+// sum of two numbers and display its sum in the console use only arrrow function
+let sum=(num1,num2)=>{
+    return num1+num2;
+}
+console.log(sum(5,6))
---
+++ task.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('eventbtn');
+const text = document.getElementById('text');
+button.addEventListener('click',function(){
+    alert('clicked')
+});
